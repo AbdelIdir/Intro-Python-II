36e5fd43f086ac5270717491577e0a79ec662fba
AbdelI <homeworkoracle@protonmail.com>
2020-04-09 17:02:15 +0200

    Created a function to pick up items from the room when the player wants it

--- src/adv.py
+++ src/adv.py
@@ -4,22 +4,21 @@ from item import Item
 
 from colored import fg, bg, attr
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+                     " 🕳 North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'foyer':    Room("Foyer", """ 🌄 Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook", """ 🏔 A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("Narrow Passage", """ 💰 The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure': Room("Treasure Chamber", """  💎 You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
@@ -48,7 +47,9 @@ room["foyer"].items.append("Knife")
 room["foyer"].items.append("Dagger")
 room["foyer"].items.append("Helmet")
 room["outside"].items.append("Potion")
-
+room["overlook"].items.append("Pickaxe")
+room["narrow"].items.append("Chicken")
+room['treasure'].items.append("Gold")
 #
 # Main
 #
@@ -88,31 +89,51 @@ while playerKey != exitButton:
     if playerKey == "n":
         if player1.current_room.n_to:
             player1.current_room = player1.current_room.n_to
+            player_action = input("What would you like to do?: ")
+            for index, item in enumerate(player1.current_room.items, start=0):
+                if item == player_action:
+                    player1.current_room.items.pop(index)
             print(
-                f"{player1.name} is in {player1.current_room}.  \n")
+                f"{player1.name} is in {player1.current_room.name}.\t {player1.current_room.description}   \n This room contains these items:\n  ")
+            print(f"{player1.current_room.items}")
             print("You have these items in your backpack 👜 : \n")
             for item in player1.backpack:
-                print(f"{item}")
+                print(f"➡{item}")
         else:
             print("%sYou cannot go there ❌🚪%s" % (fg(1), attr(0)))
     elif playerKey == "s":
         if player1.current_room.s_to:
-            player1.current_room = player1.current_room.s_to
-            print(f"{player1.name} is in {player1.current_room} These items are available in this room:{player1.current_room.items}")
+            player1.current_room = player1.current_room.n_to
+            print(
+                f"{player1.name} is in {player1.current_room.name}.\t {player1.current_room.description}   \n This room contains these items:\n  ")
+            print(f"{player1.current_room.items}")
+            print("You have these items in your backpack 👜 : \n")
+            for item in player1.backpack:
+                print(f"➡{item}")
         else:
-            print("%sYou cannot go there%s" % (fg(1), attr(0)))
+            print("%sYou cannot go there ❌🚪%s" % (fg(1), attr(0)))
     elif playerKey == "e":
         if player1.current_room.e_to:
-            player1.current_room = player1.current_room.e_to
-            print(f"{player1.name} is in {player1.current_room} These items are available in this room:{player1.current_room.items}")
+            player1.current_room = player1.current_room.n_to
+            print(
+                f"{player1.name} is in {player1.current_room.name}.\t {player1.current_room.description}   \n This room contains these items:\n  ")
+            print(f"{player1.current_room.items}")
+            print("You have these items in your backpack 👜 : \n")
+            for item in player1.backpack:
+                print(f"➡{item}")
         else:
-            print("%sYou cannot go there%s" % (fg(1), attr(0)))
+            print("%sYou cannot go there ❌🚪%s" % (fg(1), attr(0)))
     elif playerKey == "w":
         if player1.current_room.w_to:
-            player1.current_room = player1.current_room.w_to
-            print(f"{player1.name} is in {player1.current_room} These items are available in this room:{player1.current_room.items}")
+            player1.current_room = player1.current_room.n_to
+            print(
+                f"{player1.name} is in {player1.current_room.name}.\t {player1.current_room.description}   \n This room contains these items:\n  ")
+            print(f"{player1.current_room.items}")
+            print("You have these items in your backpack 👜 : \n")
+            for item in player1.backpack:
+                print(f"➡{item}")
         else:
-            print("%sYou cannot go there%s" % (fg(1), attr(0)))
+            print("%sYou cannot go there ❌🚪%s" % (fg(1), attr(0)))
     else:
         print(
             "%sType in a valid direction key: n,s,e,w%s" % (fg(1), attr(0)))
